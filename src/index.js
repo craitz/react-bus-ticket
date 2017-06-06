863c56d5815f2d9da708219eb517ba85692312d3
craitz <craitz@gmail.com>
2017-06-06 19:19:04 -0300

    Repositorio inicial

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBQq39NXj6qvnkEVmgEi3c4UZUAQ177dGs",
+  authDomain: "busticket-be05f.firebaseapp.com",
+  databaseURL: "https://busticket-be05f.firebaseio.com",
+  projectId: "busticket-be05f",
+  storageBucket: "busticket-be05f.appspot.com",
+  messagingSenderId: "246935329575"
+};
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
