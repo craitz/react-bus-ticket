ddc53c70634f811bb0ee78122c151062090d8d47
craitz <craitz@gmail.com>
2017-06-07 09:19:11 -0300

    Criado componente FormPassagem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap';
+import FormPassagem from './FormPassagem.js';
 import './App.css';
+
+const Container = ({ children }) =>
+  <Grid className="App-container">
+    <Row>
+      <Col md={6} mdOffset={3}>
+        {children}
+      </Col>
+    </Row>
+  </Grid>
+
 
 class App extends Component {
   render() {
@@ -8,9 +20,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to BusTicket</h2>
         </div>
-
+        <Container>
+          <FormPassagem></FormPassagem>
+        </Container>
       </div>
     );
   }
