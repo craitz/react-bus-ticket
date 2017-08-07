c3e5b1d4da8d1022da081a708207f19f04cfae7c
craitz <craitz@gmail.com>
2017-08-06 21:11:16 -0300

    backgrounds

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
 import { withAuth } from '../shared/hoc';
-import { Col, Jumbotron } from 'react-bootstrap';
+import { Row, Col, Jumbotron } from 'react-bootstrap';
 import DivAnimated from '../shared/DivAnimated'
 import { PageHeader } from '../shared/PageHeader';
 import store from '../store';
@@ -10,7 +10,7 @@ import FontAwesome from 'react-fontawesome';
 import Button from 'react-toolbox/lib/button/Button';
 
 export const ButtonComprar = ({ handleComprar }) =>
-  <div className="text-right welcome-button">
+  <div className="text-center welcome-button">
     <Button
       raised
       primary
@@ -44,42 +44,39 @@ class Welcome extends Component {
           {/*<PageHeaderItem tooltip="Ver histórico de compras" glyph="history" onClick={this.handlePesquisarPassagens} />*/}
         </PageHeader>
         <DivAnimated className="info-container">
-          <Col sm={8} smOffset={2}>
-            <Jumbotron className="jumbo-painel mui--z3">
-              <h2>Introdução</h2>
-              <article>
-                O BusTicket é uma SPA (Single Page Application) totalmente responsiva
-                que utiliza o conceito Mobile First para se adaptar e responder adequadamante
-                a qualquer tipo ou tamanho de tela. O objetivo da aplicação é simular
-                um guichê virtual para compra de passagens de ônibus (sem os valores
-                envolvidos na transação) para viagens entre algumas capitais do Brasil.
-              </article>
-              <hr />
-              <h2>Algumas das tecnologias utilizadas</h2>
-              <article>
-                React,
-                  Redux,
-                  JSX,
-                  Javascript (ES5,ES6,ES7),
-                  Firebase,
-                  Jest,
-                  Enzyme,
-                  TDD, DRY, SOLID, KISS,
-                  Node.js, Node Package Manager,
-                  Webpack,
-                  Babel,
-                  Boostrap, Material Design, Responsive Web Design,
-                  Mobile First,
-                  JSON,
-                  Git,
-                  CSS3,
-                  SASS,
-                  HTML5,
-                  Visual Studio 2017.
-              </article>
-              <hr />
-              <ButtonComprar handleComprar={this.handleComprarPassagem} />
-            </Jumbotron>
+          <Col sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
+            <h2>Introdução</h2>
+            <article className="intro">
+              O BusTicket é uma SPA (Single Page Application) totalmente responsiva
+              que utiliza o conceito Mobile First para se adaptar e responder adequadamante
+              a qualquer tipo ou tamanho de tela. O objetivo da aplicação é simular
+              um guichê virtual para compra de passagens de ônibus (sem os valores
+              envolvidos na transação) para viagens entre algumas capitais do Brasil.
+            </article>
+            <hr />
+            <h2>Algumas das tecnologias utilizadas</h2>
+            <article className="tech">
+              React,
+              Redux,
+              JSX,
+              Javascript (ES5,ES6,ES7),
+              Firebase,
+              Jest,
+              Enzyme,
+              TDD, DRY, SOLID, KISS,
+              Node.js, Node Package Manager,
+              Webpack,
+              Babel,
+              Boostrap, Material Design, Responsive Web Design,
+              Mobile First,
+              JSON,
+              Git,
+              CSS3,
+              SASS,
+              HTML5,
+              Visual Studio 2017.
+            </article>
+            <ButtonComprar handleComprar={this.handleComprarPassagem} />
           </Col>
         </DivAnimated>
       </div >
